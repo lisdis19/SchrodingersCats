c726fcf00f74c4a2574070bc05eebdfc9769392b
Elltawariel <anastas.fin@gmail.com>
2024-01-11 15:37:52 -0500

    ok

--- chemistry/jupyterbackend.py
+++ chemistry/jupyterbackend.py
@@ -5,16 +5,16 @@ import stat
 
 from rdkit import Chem, RDConfig, rdBase, DataStructs
 from rdkit.Chem import PandasTools, AllChem, Draw, rdMolDescriptors, GraphDescriptors, Descriptors, rdFMCS
-from rdkit.Chem.Draw import IPythonConsole, rdDepictor, rdMolDraw2D
+from rdkit.Chem.Draw import rdDepictor, rdMolDraw2D
 from rdkit.ML.Descriptors import MoleculeDescriptors
 from rdkit.Chem import MACCSkeys
 
-import matplotlib.pyplot as plt
 from rdkit.Chem import rdMolTransforms
-from rdkit.Chem.Draw import rdMolDraw2D, rdDepictor, IPythonConsole
+from rdkit.Chem.Draw import rdMolDraw2D, rdDepictor
 rdDepictor.SetPreferCoordGen(True)
 
 # Read CSV file with error handling
+
 try:
     model_csv_file = pd.read_csv('chemistry/hiv_dataset_3.csv', sep=',')  # Adjust the delimiter if needed
 except pd.errors.ParserError:
@@ -24,23 +24,27 @@ except pd.errors.ParserError:
 # Continue with the rest of your code, if applicable
 # For example, you can print the DataFrame or perform further operations
 if model_csv_file is not None:
-    print("CSV File Found!")
+    print("CSV Model File Found!")
 
-print(model_csv_file.columns)
+
+
 # first, we need to pop compounds that do not have a standard value (I saw some of them here)
 model_csv_file_retained = model_csv_file.loc[model_csv_file['Standard Value'] != None]
+model_csv_file_retained_2 = model_csv_file_retained.loc[model_csv_file_retained['Smiles'] != None]
+model_csv_file_retained_3 = model_csv_file_retained_2.loc[model_csv_file_retained_2['Standard Value'] != "NaN"]
+#print(len(model_csv_file))
+#print(len(model_csv_file_retained))
+#print(len(model_csv_file_retained_2))
+#print(len(model_csv_file_retained_3))
 
 # then we are taking smiles and standard value to a new dataframe
 column_names = ["Standard Value", "Smiles", "Molecule ChEMBL ID"]
-model_csv_file_retained_only = model_csv_file_retained[column_names]
+model_csv_file_retained_only = model_csv_file_retained_3[column_names]
 
-model_csv_file_retained_only
-model_csv_file_retained = model_csv_file.loc[model_csv_file['Standard Value'] != "NaN"]
 # Assuming model_csv_file_retained_only is your DataFrame
-model_csv_file_retained_only['Smiles'] = model_csv_file_retained_only['Smiles'].astype(str)
-
+#model_csv_file_retained_only['Smiles'] = model_csv_file_retained_only['Smiles'].astype(str)
 # Create 'Mol' column
-model_csv_file_retained_only['Mol'] = [Chem.MolFromSmiles(smile) if smile is not None else None for smile in model_csv_file_retained_only['Smiles']]
+#model_csv_file_retained_only['Mol'] = [Chem.MolFromSmiles(smile) if smile is not None else None for smile in model_csv_file_retained_only['Smiles']]
 
 mol_object_list = []
 bad_smiles = 0
@@ -49,8 +53,9 @@ for smi in model_csv_file_retained_only['Smiles']:
         mol = Chem.MolFromSmiles(smi)
         mol_object_list.append(mol)
     except:  # Print the SMILES if there was an error in converting
-        mol_object_list.append('nan')
+        mol_object_list.append('bad_molecule')
         bad_smiles += 1
         print(smi)
 print(bad_smiles)
 model_csv_file_retained_only['Mol'] = mol_object_list
+model_csv_file_retained_only_2 = model_csv_file_retained_only.loc[model_csv_file_retained_only['Mol'] != "bad_molecule"]
