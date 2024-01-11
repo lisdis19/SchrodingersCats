193d484d47647808cd1400b8342a49d7069817c3
7ronbon <7ronbon@gmail.com>
2024-01-11 15:52:55 -0500

    added comments to two files

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# root , filename.function ,  name the variable
+
 urlpatterns = [
     path("", views.home, name="home")
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse
 # View is where you allow for contents to be seen
 # Create your views here.
+
+# below is a func/method , it "renders" the html
 def home(request):
     return render(request, "home.html")
