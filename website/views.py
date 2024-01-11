959c3e2258fabc320fb29c9055d8593d3e946b50
NotEmily3203 <linemily62@gmail.com>
2024-01-11 20:31:11 +0000

    set up html

--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
-
+# View is where you allow for contents to be seen
 # Create your views here.
 def home(request):
-    return HttpResponse("hello cats!")
+    return render(request, "home.html")
