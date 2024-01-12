b0f5b9059a0aba8177981b208ea25c0aefdd971f
NotEmily3203 <linemily62@gmail.com>
2024-01-12 00:28:31 +0000

    step 2!

--- website/urls.py
+++ website/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("step2/", views.step2, name="step2"),
+    path("step1/", views.step1, name="step1")
 ]
--- website/views.py
+++ website/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render, HttpResponse
 # View is where you allow for contents to be seen
 # Create your views here.
-
+from apply_predictive_model import backendUser
 # below is a func/method , it "renders" the html
 def home(request):
     return render(request, "home.html")
+
+def step2(request):
+    return render(request, "step2.html")
+
+def step1(request):
+    return render(request, "step1.html")
+
