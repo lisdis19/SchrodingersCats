bf0f2acb50570157330bc805837e2058522e9464
NotEmily3203 <linemily62@gmail.com>
2024-01-12 18:02:24 +0000

    unfinished links

--- website/urls.py
+++ website/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.step1, name="home")
     path("", views.home, name="home"),
     path("step2/", views.step2, name="step2"),
     path("step1/", views.step1, name="step1"),
-    path("step3/", views.step3, name="step3")
+    path("step3/", views.select_algo, name="step3")
 ]
