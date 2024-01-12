d04421061e2897d497623328892b531db7a60606
NotEmily3203 <linemily62@gmail.com>
2024-01-12 06:02:08 +0000

    styling

--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, HttpResponse
-from chemistry.visual_script import upload_csv
+#from chemistry.visual_script import upload_csv
 # View is where you allow for contents to be seen
 # Create your views here.
-from apply_predictive_model import backendUser
+#from apply_predictive_model import backendUser
 # below is a func/method , it "renders" the html
 def home(request):
     return render(request, "home.html")
@@ -15,6 +15,6 @@ def step1(request):
 
 
 
-upload_csv(
-    #user_file_input
-)
+# upload_csv(
+#     #user_file_input
+# )
